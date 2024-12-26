d700f013e71bbde53a5576a98edc335fc5b6266b
Huzefah-tech <fikrihuzefah@gmail.com>
2024-12-26 13:41:48 +0700

    membuat file functions.php untuk memisahkan logic dan tampilan

--- index.php
+++ index.php
@@ -1,77 +1,5 @@
 <?php
-$host       = "localhost";
-$user       = "root";
-$pass       = "";
-$db         = "akademik";
-
-$koneksi    = mysqli_connect($host, $user, $pass, $db);
-if (!$koneksi) { //cek koneksi
-    die("Tidak bisa terkoneksi ke database");
-}
-$nim        = "";
-$nama       = "";
-$alamat     = "";
-$fakultas   = "";
-$sukses     = "";
-$error      = "";
-
-if (isset($_GET['op'])) {
-    $op = $_GET['op'];
-} else {
-    $op = "";
-}
-if($op == 'delete'){
-    $id         = $_GET['id'];
-    $sql1       = "delete from mahasiswa where id = '$id'";
-    $q1         = mysqli_query($koneksi,$sql1);
-    if($q1){
-        $sukses = "Berhasil hapus data";
-    }else{
-        $error  = "Gagal melakukan delete data";
-    }
-}
-if ($op == 'edit') {
-    $id         = $_GET['id'];
-    $sql1       = "select * from mahasiswa where id = '$id'";
-    $q1         = mysqli_query($koneksi, $sql1);
-    $r1         = mysqli_fetch_array($q1);
-    $nim        = $r1['nim'];
-    $nama       = $r1['nama'];
-    $alamat     = $r1['alamat'];
-    $fakultas   = $r1['fakultas'];
-
-    if ($nim == '') {
-        $error = "Data tidak ditemukan";
-    }
-}
-if (isset($_POST['simpan'])) { //untuk create
-    $nim        = $_POST['nim'];
-    $nama       = $_POST['nama'];
-    $alamat     = $_POST['alamat'];
-    $fakultas   = $_POST['fakultas'];
-
-    if ($nim && $nama && $alamat && $fakultas) {
-        if ($op == 'edit') { //untuk update
-            $sql1       = "update mahasiswa set nim = '$nim',nama='$nama',alamat = '$alamat',fakultas='$fakultas' where id = '$id'";
-            $q1         = mysqli_query($koneksi, $sql1);
-            if ($q1) {
-                $sukses = "Data berhasil diupdate";
-            } else {
-                $error  = "Data gagal diupdate";
-            }
-        } else { //untuk insert
-            $sql1   = "insert into mahasiswa(nim,nama,alamat,fakultas) values ('$nim','$nama','$alamat','$fakultas')";
-            $q1     = mysqli_query($koneksi, $sql1);
-            if ($q1) {
-                $sukses     = "Berhasil memasukkan data baru";
-            } else {
-                $error      = "Gagal memasukkan data";
-            }
-        }
-    } else {
-        $error = "Silakan masukkan semua data";
-    }
-}
+include 'functions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -108,7 +36,7 @@ if (isset($_POST['simpan'])) { //untuk create
                         <?php echo $error ?>
                     </div>
                 <?php
-                    header("refresh:5;url=index.php");//5 : detik
+                    header("refresh:5;url=index.php"); //5 : detik
                 }
                 ?>
                 <?php
@@ -195,14 +123,14 @@ if (isset($_POST['simpan'])) { //untuk create
                                 <td scope="row"><?php echo $fakultas ?></td>
                                 <td scope="row">
                                     <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
-                                    <a href="index.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>            
+                                    <a href="index.php?op=delete&id=<?php echo $id ?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>
                                 </td>
                             </tr>
                         <?php
                         }
                         ?>
                     </tbody>
-                    
+
                 </table>
             </div>
         </div>
